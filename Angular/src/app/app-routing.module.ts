baeddfad5e0e5d7e6eee28b190e64ba7ea5dabc1
jjordan12h <JORDAN.HAMMOND@stu.mmu.ac.uk>
2022-05-04 15:10:23 +0100

    Renamed game component to 'game-host' and add game-client

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
-import { GameComponent } from './game/game.component';
+import { GameClientComponent } from './game-client/game-client.component';
+import { GameHostComponent } from './game-host/game-host.component';
 import { HelpComponent } from './help/help.component';
 import { HomeComponent } from './home/home.component';
 import { MediaComponent } from './media/media.component';
@@ -12,7 +13,8 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'media', component: MediaComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'game', component: GameComponent}
+  {path: 'game-host', component: GameHostComponent},
+  {path: 'game-client', component: GameClientComponent}
 ];
 
 @NgModule({
