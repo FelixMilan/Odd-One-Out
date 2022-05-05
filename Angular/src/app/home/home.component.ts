b0cb95ae1f1fb6fa3ade040039798e32aa12c475
jjordan12h <JORDAN.HAMMOND@stu.mmu.ac.uk>
2022-05-05 01:45:28 +0100

    Deleted more uneccesary files and imports for issue fix

--- Angular/src/app/home/home.component.ts
+++ Angular/src/app/home/home.component.ts
@@ -1,8 +1,6 @@
-import { PLAYERS } from './../players';
 import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
 import { prepareSyntheticPropertyName } from '@angular/compiler/src/render3/util';
 import { Component, OnInit } from '@angular/core';
-import { Player } from '../player';
 
 @Component({
   selector: 'app-home',
--- Angular/src/app/randomisor.service.ts
+++ Angular/src/app/randomisor.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Name } from './name';
 
 @Injectable({
   providedIn: 'root'
@@ -8,20 +7,9 @@ import { Name } from './name';
 export class RandomisorService {
 
   private url = 'api/namePool';
-  namePool: Name[] = [];
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
-
-  getNamePool(): void {
-    let namePool = this.http.get<Name[]>(this.url);
-    namePool.subscribe(namePool => this.namePool = namePool);
-  }
-
-  getRandomNameFromPool(): Name {
-    let chosenIndex: number = this.getRandomInt(0, this.namePool.length);
-    return this.namePool[chosenIndex];
-  }
 
   getRandomInt(min: number, max: number): number {
     min = Math.ceil(min);
