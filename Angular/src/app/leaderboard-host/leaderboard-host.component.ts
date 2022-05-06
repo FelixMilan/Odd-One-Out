4733553f9d5419b9ee33d1bf6170051e6e859ad1
FelixMilan <felix.m.p.milan@gmail.com>
2022-05-06 03:26:59 +0100

    leaderbboard changes

--- Angular/src/app/leaderboard-host/leaderboard-host.component.ts
+++ Angular/src/app/leaderboard-host/leaderboard-host.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServerService } from '../server.service';
 
 @Component({
   selector: 'app-leaderboard-host',
@@ -9,13 +10,21 @@ export class LeaderboardHostComponent implements OnInit {
 
   oddOneOutPlayer: string='';
   oddOneOutCeleb: string='';
+  oddOneOutDrawing: string='';
 
   playerCorrect=[]
   playerIncorrect=[];
 
-  constructor() { }
+  constructor(private serverService: ServerService) { }
 
   ngOnInit(): void {
+    const oddOneOutDrawingId = this.serverService.getGameState().oddOneOut;
+    const drawings = this.serverService.getGameState().drawings;
+    const drawing = drawings.find((d: any) => d.id === oddOneOutDrawingId);
+
+    this.oddOneOutPlayer = drawing?.playerName ?? "UNKNOWN";
+    this.oddOneOutCeleb = drawing?.celebName ?? "UNKNOWN";
+    this.oddOneOutDrawing = drawing?.drawing ?? "";
   }
 
 }
