627e72191fe9dd6341f4bedf7de0b690917e562e
jjordan12h <JORDAN.HAMMOND@stu.mmu.ac.uk>
2022-05-06 02:18:35 +0100

    Added timer to writing-screen-host

--- Angular/src/app/writing-screen-host/writing-screen-host.component.ts
+++ Angular/src/app/writing-screen-host/writing-screen-host.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServerService } from '../server.service';
 
 @Component({
   selector: 'app-writing-screen-host',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WritingScreenHostComponent implements OnInit {
 
-  constructor() { }
+  constructor(private serverService: ServerService) { }
+
+  checkInterval: any=null;
+  lobbyCode?: string;
+  timeLeft?: number;
 
   ngOnInit(): void {
+    this.lobbyCode = this.serverService.getLobbyCode();
+
+    this.checkInterval = setInterval(() => {
+      this.timeLeft = this.serverService.getGameState().timeLeft;
+    }, 500);
+  }
+
+  ngOnDestroy(): void {
+    if (this.checkInterval) {
+      clearInterval(this.checkInterval);
+    }
   }
 
 }
